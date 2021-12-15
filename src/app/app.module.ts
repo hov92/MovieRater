ce5ddadf43c5b45fb2183e4911ddfaf1bae7ed97
Dwayne Hovington <dhov92@outlook.com>
2021-12-15 16:55:22 -0500

    almost there

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+
+
+import { AuthModule} from './auth/auth.module'
+import { MainModule} from './main/main.module'
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'auth'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AuthModule,
+    MainModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ApiService } from '../api.service';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { MainComponent } from './main.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { ReactiveFormsModule} from '@angular/forms'
+
+
+
+const routes: Routes = [
+  {path: 'movies', component: MainComponent}
+];
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    MovieListComponent,
+    MovieDetailsComponent,
+    MovieFormComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    FontAwesomeModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    ApiService
+  ]
+})
+export class MainModule { }
